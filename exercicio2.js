13fa9d27b3591d03e11d5cc50c7c8d1d3c0976b6
jungesla <jungeslarissaamanda@gmail.com>
2024-05-14 21:39:54 -0300

    Exercise class 2 - ProductManager class with addProduct and getProductById

---
+++ exercicio2.js
@@ -0,0 +1,47 @@
+class ProductManager {
+    constructor() {
+        this.products = [];
+        this.productIdCounter = 1;
+    }
+
+    addProduct(title, description, price, thumbnail, code, stock) {
+        if (!title || !description || !price || !thumbnail || !code || !stock) {
+            console.error("Todos os campos são obrigatórios");
+            return;
+        }
+
+        if (this.products.some(product => product.code === code)) {
+            console.error("O código do produto já existe");
+            return;
+        }
+
+        const product = {
+            id: this.productIdCounter++,
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock
+        };
+
+        this.products.push(product);
+        console.log("Produto adicionado com sucesso:", product);
+    }
+
+    getProductById(id) {
+        const product = this.products.find(product => product.id === id);
+        if (product) {
+            console.log("Produto encontrado:", product);
+        } else {
+            console.error("Não encontrado", product);
+        }
+    }
+}
+
+
+const productManager = new ProductManager();
+productManager.addProduct("Brinco", "Brinco prata flor jasmin", 89.55, "documents/larissa/brincoJasmin.jpg", "001", 50);
+productManager.addProduct("Bota", "Bota arezzo preta", 139.50, "documnets/larissa/botaArezzoPreta.jpg", "002", 30);
+productManager.getProductById(1);
+productManager.getProductById(11);
